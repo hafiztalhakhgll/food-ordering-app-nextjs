b32a9d074c6b7997361abb86546d2927813dae18
Talha Khalid <talha.khalid@greenlightlabs.tech>
2022-11-08 17:26:17 +0500

    Working on the details of Product

--- components/PizzaCard.jsx
+++ components/PizzaCard.jsx
@@ -1,11 +1,16 @@
 import Image from "next/image";
+import Link from "next/link";
 import styles from "../styles/PizzaCard.module.css";
 
 function PizzaCard() {
   return (
     <div className={styles.container}>
       <Image className={styles.pizzaImg} src="/img/pizza.png" alt="" width="500" height="500" />
-      <h1 className={styles.title}>FIORI DI ZUCCA</h1>
+      <h1 className={styles.title}>
+        <Link legacyBehavior href={"product/1"}>
+          <a>FIORI DI ZUCCA</a>
+        </Link>
+      </h1>
       <span className={styles.price}>$19.90</span>
       <p className={styles.desc}>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
     </div>
