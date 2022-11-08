ceb6dfc94a5d4a564fc41fb7b929238d45b7a639
Talha Khalid <talha.khalid@greenlightlabs.tech>
2022-11-08 16:47:22 +0500

    Featured section completed

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import Head from "next/head";
-import Image from "next/image";
+import Featured from "../components/Featured";
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
@@ -11,7 +11,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <h1>Homepage</h1>
+      <Featured />
     </div>
   );
 }
